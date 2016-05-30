fbb850f61f0caaecab4426f2010e151a105b6f7d
ahmed <a.raghebmail@yahoo.com>
2016-05-30 19:10:30 +0200

    Test Project

---
+++ redisrepository/RedisRepository.Test/RedisClientTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedisRepository.Test
+{
+    [TestClass]
+    public class RedisClientTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
